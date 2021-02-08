9c780fc655c34d18dc8d07ea7d1db42f5e12b5e7
aarongermaine <vcnts0@gmail.com>
2021-02-08 15:59:20 -0700

    JS and Api fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,14 +13,24 @@ var weatherDisplayEl = document.querySelector("#weather-content");
 
 var warningIconEl = document.querySelector("#warning");
 
+var local;
+
+let forecastAlign = [];
+
+let history = ["Denver"];
+
 //this function determines validity
 var submitClick = function (event) {
   event.preventDefault();
-  var cityID = citySubmit.value;
-  console.log(cityID);
-  if (cityID) {
-    cityGetter(cityID);
 
+  var cityID = citySubmit.value;
+
+  if (cityID !== null) {
+    cityGetter(cityID);
+    history.some((h) =>
+      h === cityID ? console.log("Invalid") : history.push(cityID)
+    );
+    localStorage.setItem("history", JSON.stringify(history));
     cityNameEl.textContent = cityID;
 
     cityNameEl.value = "";
@@ -29,25 +39,63 @@ var submitClick = function (event) {
   }
 };
 
-var cityGetter = function (cityID) {
+var cityGetter = async function (cityID) {
   var weatherApiKey = "aac7a25ab14d5f437c627c978531f784";
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
 
   fetch(
-    "https://api.openweathermap.org/data/2.5/forecast?q=" +
-      cityID +
-      "&appid=" +
-      weatherApiKey
+    `https://api.openweathermap.org/data/2.5/forecast?q=${cityID}&appid=${weatherApiKey}&units=imperial`,
+    requestOptions
   )
-    .then(function (resp) {
-      console.log(resp.json());
-      return resp.json();
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+
+      // Weather Search History
+      var localHistory = JSON.parse(localStorage.getItem("history"));
+
+      var historyUl = document.getElementById("weather-history");
+      var historyList = document.createElement("li");
+      historyList.setAttribute("class", "list-group-item");
+      historyList.innerHTML = null;
+      localHistory.map((history) => {
+        historyList.innerHTML = `${history}`;
+        return historyUl.appendChild(historyList);
+      });
+
+      // Weather Cards
+      var weatherDash = document.getElementById("weather-dash");
+
+      var data = result.list;
+
+      forecastAlign.push(data[0], data[7], data[15], data[23], data[31]);
+
+      forecastAlign.map((index) => {
+        var weatherCol = document.createElement("div");
+        weatherCol.setAttribute("class", "col-2");
+        var weatherCard = document.createElement("div");
+        weatherCard.setAttribute("class", "card");
+        var format = (i) =>
+          new Date(i).toLocaleDateString("en-us", { weekday: "long" });
+        weatherCard.innerHTML = `
+        <div class="card-header bg-transparent">${format(
+          index.dt_txt
+        )} <img src="../../assets/images/${index.weather[0].icon}.png" alt="${
+          index.weather.description
+        }"/></div>
+        <ul class="list-group list-group-flush"><li class="list-group-item"><strong>Low: </strong>${Math.floor(
+          index.main.temp_min
+        )}</li><li class="list-group-item"> <strong>High: </strong>${Math.round(
+          index.main.temp_max
+        )}</li></ul>`;
+        weatherCol.appendChild(weatherCard);
+        return weatherDash.appendChild(weatherCol);
+      });
     })
-    .then(function (data) {
-      showWeather(data);
-    })
-    .catch(function () {
-      weatherDisplayEl.textContent = "That's not real";
-    });
+    .catch((error) => console.log("error", error));
 };
 
 var showWeather = function (cityID) {
@@ -55,5 +103,9 @@ var showWeather = function (cityID) {
     return;
   }
 };
+
+function clearForecast() {
+  document.getElementById("weather-dash").innerHTML = "";
+}
 
 cityForm.addEventListener("submit", submitClick);
